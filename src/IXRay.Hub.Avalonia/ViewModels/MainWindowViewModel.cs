ce1e100edbfc1819969b26aaf400429786d88ff7
Drombeys <dima.zolotov02@gmail.com>
2024-02-13 16:12:45 +0300

    Add message window
    With window navigation

--- src/IXRay.Hub.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/IXRay.Hub.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -8,15 +8,21 @@ namespace IXRay.Hub.Avalonia.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private readonly IWindowManager _windowManager;
+    private readonly ViewModelLocator _viewModelLocator;
+
     public INavigationService NavigationService { get; set; }
 
     public ReactiveCommand<Unit, Unit>? NavigateToHomeCommand { get; set; }
     public ReactiveCommand<Unit, Unit>? NavigateToSettingsCommand { get; set; }
+    public ReactiveCommand<Unit, Unit>? OpenMessageWindowCommand { get; set; }
 
-    public MainWindowViewModel(INavigationService navigationService)
+    public MainWindowViewModel(INavigationService navigationService, IWindowManager windowManager, 
+        ViewModelLocator viewModelLocator)
     {
         NavigationService = navigationService;
-
+        _windowManager = windowManager;
+        _viewModelLocator = viewModelLocator;
         SetupBinding();
     }
 
@@ -29,6 +35,10 @@ public class MainWindowViewModel : ViewModelBase
         NavigateToSettingsCommand = ReactiveCommand.Create<Unit>(execute =>
         {
             NavigationService.NavigateTo<SettingsViewModel>();
+        });
+        OpenMessageWindowCommand = ReactiveCommand.Create<Unit>(execute =>
+        {
+            _windowManager.ShowWindow(_viewModelLocator.MessageWindowViewModel);
         });
     }
 }
---
+++ src/IXRay.Hub.Avalonia/Views/MessageWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace IXRay.Hub.Avalonia;
+
+public partial class MessageWindow : Window
+{
+    public MessageWindow()
+    {
+        InitializeComponent();
+    }
+}
