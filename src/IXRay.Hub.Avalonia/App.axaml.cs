71762f0c305c4953c3f3fb8ed5df5a6d9f7c303f
Drombeys <dima.zolotov02@gmail.com>
2024-02-13 11:27:30 +0300

    Integrate Microsoft dependency injection

--- src/IXRay.Hub.Avalonia/App.axaml.cs
+++ src/IXRay.Hub.Avalonia/App.axaml.cs
@@ -5,10 +5,20 @@ using Avalonia.Markup.Xaml;
 using IXRay.Hub.Avalonia.ViewModels;
 using IXRay.Hub.Avalonia.Views;
 
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IXRay.Hub.Avalonia;
 
 public partial class App : Application
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
+    {
+        _serviceProvider = ConfigureServices()
+               .BuildServiceProvider();
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,12 +28,22 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
+            desktop.MainWindow = _serviceProvider.GetRequiredService<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static ServiceCollection ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton(provider => new MainWindow
+        {
+            DataContext = provider.GetRequiredService<MainWindowViewModel>()
+        });
+        services.AddSingleton<MainWindowViewModel>();
+
+        return services;
     }
 }
