fd97cbb44971b7e0d1e996194024bbbc3397b0ef
Drombeys <dima.zolotov02@gmail.com>
2024-02-22 21:40:15 +0300

    Implement `CloseWindow` method

--- src/IXRay.Hub.Avalonia/Services/IWindowManager.cs
+++ src/IXRay.Hub.Avalonia/Services/IWindowManager.cs
@@ -1,8 +1,12 @@
-﻿using IXRay.Hub.Avalonia.ViewModels;
+using Avalonia.Controls;
+
+using IXRay.Hub.Avalonia.ViewModels;
 
 namespace IXRay.Hub.Avalonia.Services;
 
 public interface IWindowManager
 {
+    Window? GetWindow(ViewModelBase viewModel);
     void ShowWindow(ViewModelBase viewModel);
+    void CloseWindow(ViewModelBase viewModel);
 }
--- src/IXRay.Hub.Avalonia/Services/WindowManager.cs
+++ src/IXRay.Hub.Avalonia/Services/WindowManager.cs
@@ -1,25 +1,54 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
 
 using IXRay.Hub.Avalonia.ViewModels;
 
 namespace IXRay.Hub.Avalonia.Services;
 
+/// <summary>
+/// Manages windows based on provided view models
+/// </summary>
+/// <param name="windowMapper">Window mapper instance to be used for retrieving window types</param>
 public class WindowManager(WindowMapper windowMapper) : IWindowManager
 {
     private readonly WindowMapper _windowMapper = windowMapper;
 
-    public void ShowWindow(ViewModelBase viewModel)
+    /// <summary>
+    /// Retrieves window associated with provided view model and sets its data context
+    /// </summary>
+    /// <param name="viewModel">View model for which to retrieve window</param>
+    /// <returns>Window associated with provided view model, or <c>null</c> if window type is not found</returns>
+    /// <exception cref="ArgumentException">Thrown when window type is not found for provided view model</exception>
+    /// <exception cref="InvalidOperationException">Thrown when failed to create instance of window</exception>
+    public Window? GetWindow(ViewModelBase viewModel)
     {
+        // Get the window type for the provided view model from WindowMapper
         var windowType = _windowMapper.GetWindowTypeForViewModel(viewModel.GetType());
+
+        // If window type is not found, throw an ArgumentException
         if (windowType is null) {
-            return;
+            throw new ArgumentException("Window type not found for the provided view model", nameof(viewModel));
         }
 
+        // Create an instance of the window using Activator
         if (Activator.CreateInstance(windowType) is not Window window) {
-            return;
+            // If failed to create an instance of the window, throw InvalidOperationException
+            throw new InvalidOperationException("Failed to create an instance of window");
         }
 
+        // Set the data context for the window
         window.DataContext = viewModel;
-        window.Show();
+        return window;
     }
+
+    /// <summary>
+    /// Shows window associated with provided view model
+    /// </summary>
+    /// <param name="viewModel">View model for which to show window</param>
+    public void ShowWindow(ViewModelBase viewModel) => GetWindow(viewModel)?.Show();
+
+    /// <summary>
+    /// Closes window associated with provided view model
+    /// </summary>
+    /// <param name="viewModel">View model for which to close window</param>
+    public void CloseWindow(ViewModelBase viewModel) => GetWindow(viewModel)?.Close();
 }
