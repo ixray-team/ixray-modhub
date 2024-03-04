bef339d0ca60120de0656c22ba2877d6be3762a1
Drombeys <dima.zolotov02@gmail.com>
2024-03-04 18:46:12 +0300

    Implement logging support

---
+++ src/IXRay.Hub.Avalonia/Helpers/SystemInfoPrinter.cs
@@ -0,0 +1,20 @@
+﻿namespace IXRay.Hub.Avalonia.Helpers;
+
+public static class SystemInfoPrinter
+{
+    public static void PrintOsInfo(Serilog.ILogger logger)
+    {
+        logger.Information("OS: {OSVersion}", Environment.OSVersion);
+        logger.Information("Processor architecture: {Architecture}",
+            Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+        logger.Information("User / PC: {UserName} / {MachineName}", Environment.UserName,
+            Environment.MachineName);
+        logger.Information("System directory: {SystemDirectory} {NewLine}", Environment.SystemDirectory, Environment.NewLine);
+        logger.Information(".NET: {NETVersion}", Environment.Version);
+        logger.Information("ProcessId: {ProcessId}", Environment.ProcessId);
+        logger.Information("Processor count: {ProcessorCount}", Environment.ProcessorCount);
+        logger.Information("Process path: {ProcessPath}", Environment.ProcessPath);
+        logger.Information("Current directory: {CurrentDirectory}", Environment.CurrentDirectory);
+        logger.Information("Application directory: {BaseDirectory} {NewLine}", AppDomain.CurrentDomain.BaseDirectory, Environment.NewLine);
+    }
+}
